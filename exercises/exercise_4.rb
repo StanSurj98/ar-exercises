6284a43758655026c18605b1cce45c3e0c03f4b2
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-18 12:33:50 -0600

    ex4 finished: conditional querying of database

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,59 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Creating 3 more stores..."
+
+data = [
+  {:name => "Surrey", :annual_revenue => 224000, :mens_apparel => false, :womens_apparel => true},
+  {:name => "Whistler", :annual_revenue => 1900000, :mens_apparel => true, :womens_apparel => false},
+  {:name => "Yaletown", :annual_revenue => 430000, :mens_apparel => true, :womens_apparel => true},
+]
+
+data.each do |object|
+  Store.create do |store|
+    store.name = object[:name]
+    store.annual_revenue = object[:annual_revenue]
+    store.mens_apparel = object[:mens_apparel]
+    store.womens_apparel = object[:womens_apparel]
+  end
+end
+
+p Store.count
+# p Store.all
+
+
+puts "Fetching stores that only carry mens apparel..."
+puts "----------"
+puts
+# Should be id: 1, 3, 5, 6 (but 3 was deleted)
+@mens_stores = Store.where(mens_apparel: true)
+p @mens_stores
+
+
+puts "Outputting the name & annual revenue for each mens stores..."
+puts "----------"
+puts
+
+@mens_stores.each do |store|
+  print "Name: #{store.name}, "
+  puts "Annual Revenue: $#{store.annual_revenue}"
+end
+
+
+puts "Fetching women stores that make under $1M in annual revenue..."
+puts "----------"
+puts
+
+# To protect against SQL injections, using ("string conditional", values-for-?)
+@womens_stores = Store.where(
+  "womens_apparel = ? AND annual_revenue < ?", true, 1000000
+)
+
+# p @womens_stores
+
+@womens_stores.each do |store|
+  print "Name: #{store.name}, "
+  puts "Annual Revenue: $#{store.annual_revenue}"
+end
+# Burnaby, Surrey, Yaletown
