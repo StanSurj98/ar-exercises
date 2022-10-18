70ed7cee9faf7655c030067ca41cd32193ad4a1b
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-18 13:31:03 -0600

    first attempt at adding validation to the Store and Employee models

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,15 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than: 0 }
+
+  # custom validation method
+  validate :must_carry_mens_or_womens_apparel
+
+  def must_carry_mens_or_womens_apparel
+    if !mens_apparel? && !womens_apparel?
+      errors.add("stores must carry at least one of mens_apparel, or womens_apparel")
+    end
+  end
+  
 end
