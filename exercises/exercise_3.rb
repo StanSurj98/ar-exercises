bb95939e0988eea4dcea28e062968085ebb32a62
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-18 12:04:41 -0600

    ex3 deleting @store3 exercise completed

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,15 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+
+@store3 = Store.find_by(id: 3)
+p @store3
+
+puts "Deleting Store 3..."
+puts "----------"
+
+@store3.destroy
+
+puts "Current Store.count: "
+puts Store.count
+
