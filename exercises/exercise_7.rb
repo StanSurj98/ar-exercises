a1e92bc0aa816a85b264b58fbc87f50d775e884e
Stanley Surjanto <stan.surjanto@gmail.com>
2022-10-18 13:49:50 -0600

    ex7 completed: learning validation and raising validation errors. Build custom validation methods for models.

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -8,5 +8,11 @@ require_relative './exercise_6'
 
 puts "Exercise 7"
 puts "----------"
-
+puts
 # Your code goes here ...
+puts "Trying to add stores that fail validation..."
+
+# Adding ! raises the error, without it -> there is an error but it won't pop
+@store7 = Store.create!(name: "Ab") 
+@store7 = Store.create!(annual_revenue: 0)
+@store7 = Store.create!(mens_apparel: false, womens_apparel: false)
--- lib/store.rb
+++ lib/store.rb
@@ -7,9 +7,10 @@ class Store < ActiveRecord::Base
   validate :must_carry_mens_or_womens_apparel
 
   def must_carry_mens_or_womens_apparel
-    if !mens_apparel? && !womens_apparel?
-      errors.add("stores must carry at least one of mens_apparel, or womens_apparel")
+    if !mens_apparel && !womens_apparel
+      # usually it's :{field_name_that_error_tracks}, but since this boolean thing, target :base for just overall the entire model
+      errors.add(:base, "stores must carry at least one of mens_apparel, or womens_apparel")
     end
   end
-  
+
 end
